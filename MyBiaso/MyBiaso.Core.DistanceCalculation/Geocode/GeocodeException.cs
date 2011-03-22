15782ba1361bdb842cf893731d3d737eafe122dc
Sebastian Martens <sebastian.martens@gmx.de>
2011-03-22 11:24:25 +0100

    Improved distance calculation with origin and destination

---
+++ MyBiaso/MyBiaso.Core.DistanceCalculation/Geocode/GeocodeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyBiaso.Core.DistanceCalculation.Geocode {
+
+    /// <summary>
+    /// Ausnahme wird ausgelöst, wenn die Geokodierung fehlschlägt.
+    /// </summary>
+    public class GeocodeException:Exception {
+        
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="message">Nachricht</param>
+        /// <param name="exception">Ausnahme</param>
+        public GeocodeException(string message, Exception exception)
+            : base(message, exception) {
+
+        }
+    }
+}
